452341d60ebd76b3d4591b69c9797de5c7207f37
Elizabeth Case <elizabethhcase@gmail.com>
2014-02-15 14:27:40 -0800

    chart responsive

--- chart.js
+++ chart.js
@@ -1,9 +1,9 @@
 google.load("visualization","1", {packages:["corechart"]});
 google.setOnLoadCallback(drawChart);
 
-function drawChart() {
+function drawChart(){
     var data = google.visualization.arrayToDataTable([
-        ['Name','After','Before'],
+        ['Name','After','Before',],
         ['Ada Beatriz',4.25,{v:5.25, f:'9.5'}],
         ['Cristo',9,{v:18.75, f:'27.75'}],
         ['Didier Amaury',15.25,{v:21.75, f:'37'}],
@@ -13,16 +13,22 @@ function drawChart() {
         ['7 Mares',10.25,{v:57.75, f:'68'}],
     ]);
 
-    var options ={
-        title: 'Carbon Monoxide levels on each boat before and after the intervention',
-        hAxis: {title: 'CO Parts per million (PPM)', viewWindow: {max:70} },
-        isStacked: true,
-        animation: {duration: 1000, easing: 'out'},
-        backgroundColor: {fill:'transparent', stroke: '#000000', strokeWidth: 5},
-        height: 400,
-        width: 500,
-    };
+var options ={
+    title: 'Carbon Monoxide levels on each boat before and after the intervention',
+    titleTextStyle: {color: 'white', fontSize: 16, bold: true},
+    hAxis: {title: 'CO Parts per million (PPM)', viewWindow: {max:70}, textStyle: {color:'white'}, titleTextStyle: {color: 'white'} },
+    vAxis: {gridlines: {color:'white'}, textStyle: {color:'white'}},
+    isStacked: true,
+    animation: {duration: 1000, easing: 'out'},
+    backgroundColor: {fill:'transparent', stroke: '#000000', strokeWidth: 5},
+    height: 400, 
+    width: '90%'   
+};
 
     var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
     chart.draw(data, options);
+
+    window.onresize = function() {
+        chart.draw(data, options);
+    };
 }
