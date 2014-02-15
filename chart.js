29e7114cc44fad05320696af09c55a4bd92298e5
Elizabeth Case <elizabethhcase@gmail.com>
2014-02-15 15:36:50 -0800

    adding safety line to chart

--- chart.js
+++ chart.js
@@ -3,14 +3,14 @@ google.setOnLoadCallback(drawChart);
 
 function drawChart(){
     var data = google.visualization.arrayToDataTable([
-        ['Name','After','Before',],
-        ['Ada Beatriz',4.25,{v:5.25, f:'9.5'}],
-        ['Cristo',9,{v:18.75, f:'27.75'}],
-        ['Didier Amaury',15.25,{v:21.75, f:'37'}],
-        ['Julissa',5,{v:14, f:'19'}],
-        ['Vivianita',2,{v:2, f:'4'}],
-        ['Yvonne',3.5,{v:10, f:'13.5'}],
-        ['7 Mares',10.25,{v:57.75, f:'68'}],
+        ['Name','After','Before','Safety recommendation', {type:'string',role:'tooltip'}],
+        ['Ada Beatriz',4.25,{v:5.25, f:'9.5'},10,'DPSM safety rec = 10 ppm'],
+        ['Cristo',9,{v:18.75, f:'27.75'},10,'DPSM safety rec = 10 ppm'],
+        ['Didier Amaury',15.25,{v:21.75, f:'37'},10,'DPSM safety rec = 10 ppm'],
+        ['Julissa',5,{v:14, f:'19'},10,'DPSM safety rec = 10 ppm'],
+        ['Vivianita',2,{v:2, f:'4'},10,'DPSM safety rec = 10 ppm'],
+        ['Yvonne',3.5,{v:10, f:'13.5'},10,'DPSM safety rec = 10 ppm'],
+        ['7 Mares',10.25,{v:57.75, f:'68'},10,'DPSM safety rec = 10 ppm'],
     ]);
 
 var options ={
@@ -22,7 +22,9 @@ var options ={
     animation: {duration: 1000, easing: 'out'},
     backgroundColor: {fill:'transparent', stroke: '#000000', strokeWidth: 5},
     height: 400, 
-    width: '90%'   
+    width: '90%',
+    seriesType:"bars",
+    series: {2: {type: "line", color: "yellow", focusTarget: 'multiple'}},   
 };
 
     var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
