5937ec76937cf58a6ef55f3fc8ae4fb2bd3dbff3
Ryan Hansberry <rhansby@gmail.com>
2014-02-15 15:06:00 -0800

    Made container move vertically as video resizes

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 window.onload = function() {
+    var video = document.getElementById('video-yucatan'),
+        container = document.getElementById('container');
+
+    var repositionContainer = function() {
+        container.style.top = video.offsetHeight + 'px';
+    };
+    repositionContainer();
+
+    window.onresize = function() {
+        repositionContainer();
+    }
 }
