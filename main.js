511f807c857ad887006df8b2a27d00ed8a5d0135
Ryan Hansberry <rhansby@gmail.com>
2014-02-15 16:41:26 -0800

    Changed header video to a darker one

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 window.onload = function() {
     var video = document.getElementById('video-yucatan'),
+        video_overlay = document.getElementById('video-overlay'),
         container = document.getElementById('container');
 
     var repositionContainer = function() {
@@ -14,5 +15,7 @@ window.onload = function() {
     window.onscroll = function() {
         var alpha = 1 - (window.scrollY / video.offsetHeight); // A ratio of how far it has scrolled
         video.style.opacity = alpha;
+
+        video_overlay.style.bottom = window.scrollY;
     }
 }
