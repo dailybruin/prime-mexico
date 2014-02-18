a5c8a5d9741a47df1b361a0ebb06978ed53253bb
Connie Chiou <connie.chiou@yahoo.com>
2014-02-17 18:47:38 -0800

    added reefs (need parallax)

--- main.js
+++ main.js
@@ -51,6 +51,17 @@ $(document).ready(function() {
     }
 
     sub_header = document.getElementById('sub-header');
+	
+	var inView = function isScrolledIntoView(elem) {
+		var docViewTop = $(window).scrollTop();
+		var docViewBottom = docViewTop + $(window).height();
+
+		var elemTop = $(elem).offset().top;
+		var elemBottom = elemTop + $(elem).height();
+
+		return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+	}
+	
     window.onscroll = function() {
         var scroll_position = $window.scrollTop(),
             client_height = $window.height(),
@@ -67,13 +78,6 @@ $(document).ready(function() {
         $('#sub-header').css(
             'position', (scroll_position >= client_height) ? 'fixed' : 'absolute'
         );
-    }
-	
-    // Call once to reposition stuff:
-    window.onresize();
-});
-
-document.onscroll = function() {
 		var mainOffset = $('#main').offset().top;
 		var windowWidth = window.innerWidth;
 		
@@ -84,4 +88,8 @@ document.onscroll = function() {
 
 		$('#diver').css('margin-left', (windowWidth)/50);		
 		$('#diver').css('top', (window.scrollY-mainOffset+100)*1.04 + 'px');
-}
+    }
+	
+    // Call once to reposition stuff:
+    window.onresize();
+});
